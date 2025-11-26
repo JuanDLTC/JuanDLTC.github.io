f07d2e416ab04ce610daf2f0602149e0f6e3828b
Juan De la Torre <10880168@uvu.edu>
2025-11-25 20:33:54 -0700

    project 7
    "Finished project 7"

---
+++ Projects/Project7/myscripts.js
@@ -0,0 +1,55 @@
+// JavaScript Document
+
+PrintButton1 = new Image
+	PrintButton2 = new Image
+	logo1 = new Image
+	logo2 = new Image
+if (document.images) {
+	PrintButton1.src = "images/Button1.png"
+	PrintButton2.src = "images/Button2.png"
+	logo1.src = "images/Logo1.png"
+	logo2.src = "images/Logo2.png"
+}
+
+
+		var imgArray = new Array(
+			'CoupleHikingLg.jpg',
+			'ScubaLg.jpg',
+			'OlderBikingLarge.jpg',
+			'CoupleFlyFishingLg.jpg'
+
+		);
+
+
+		var titleArray = new Array(
+			'Couple Hiking',
+			'Couple Scuba Diving ',
+			'Older Couple Biking',
+			'Couple Fly Fishing'
+			
+		);
+			
+		var imgPath = "images/fullsize/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+      			textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
